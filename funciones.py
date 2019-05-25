0294ca733e9db41eb9870d418716d93a9538a397
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2019-05-24 22:33:17 -0600

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,6 +1,6 @@
 #Hoja de Trabajo 10
 #Juan Diego Solorzano 18151
-#Andrea Paniagua
+#Andrea Paniagua 18
 
 from py2neo import *
 from neo4j import *
@@ -15,13 +15,11 @@ def crearUsuario(nombre, edad):
 
 def crearRating(nombre, lugar, leGusto):
     if leGusto == "si":
-        db.run("MATCH (u:Usuario), (p:Place) WHERE u.name = '"+nombre+"' AND p.name = '"+lugar+"' CREATE (u)-[:LIKES]-> (p)")
+        db.run("MATCH (u:Usuario), (p:Place) WHERE u.nombre = '"+nombre+"' AND p.nombre = '"+lugar+"' CREATE (u)-[:LIKES]-> (p)")
 def verificar(resul):
-    resultado = "Te sugerimos visitar los siguientes lugares: \n"
+
     if len(resul) != 0:
-        
-        for i in act:
-            resultado = resultado + "" + i + "\n"
+        resultado = resul
         
     elif len(resul) == 0:
         resultado = "No se encontraron recomendaciones"
@@ -31,39 +29,157 @@ def verificar(resul):
 
 def recomendacion(dinero, compan, actividad):
 
+    #Encuentra segun tipo de actividad
     act = []
-    nombres = db.run("MATCH (p:Place)-[:ES]->(t:Tourism) WHERE t.type = '"+actividad+"' RETURN p.name").to_ndarray()
-    
-    for i in nombres:
-            nombres = str(nombres[i])
-            nomb = nombres.replace('[',"")
-            nomb1 = nomb.replace(']',"")
-            nomb2 = nomb1.replace("'","")
-            act.append(nomb2)
+    nombres = db.run("MATCH (p:Place)-[:ES]->(t:Tourism) WHERE t.type = '"+actividad+"' RETURN p.nombre").to_ndarray()
 
-    
+    nombre = str(nombres)
+    nomb = nombre.replace('[',"")
+    nomb = nomb.replace("']","*")
+    nomb = nomb.replace("]","")
+    nomb = nomb.replace("'","")
+    nomb = nomb.replace("\n","")
+    no = nomb.split("*")
+    x = 1
+    entero = ""
+    for i in no:
+        if x != 11:
+            entero = entero + str(x)+". " + i + "\n"
+            x = x + 1
+    act.append(str(entero))
 
-    doc = db.run("MATCH (p:Place)-[:TIENE]->(b:Budget) WHERE b.type = '"+dinero+"' RETURN p.name").to_ndarray()
-    for i in doc:
-        for x in act:
-            if str(i) == "['"+str(x)+"']":
-                doc = str(doc[i])
-                doc1 = doc.replace('[',"")
-                doc2 = doc1.replace(']',"")
-                doc3 = doc2.replace("'","")
-                act.append(doc3)
+    #Encuentra segun presupuesto
+    doc = db.run("MATCH (p:Place)-[:TIENE]->(b:Budget) WHERE b.type = '"+dinero+"' RETURN p.nombre").to_ndarray()
+
+    docs = str(doc)
+    docs= docs.replace('[',"")
+    docs = docs.replace("']","*")
+    docs = docs.replace(']',"")
+    docs = docs.replace("'","")
+    docs = docs.replace("\n","")
+    docs = docs.split("*")
+    x = 1
+    entero = ""
+
+    for i in docs:
+        if x != 11:
+            entero = entero + str(x)+". " + i + "\n"
+            x = x + 1
+
+    act.append(str(entero))
 
 
-    doc4 = db.run("MATCH (p:Place)-[:IR]->(c:Companion) WHERE c.type = '"+compan+"' RETURN p.name").to_ndarray()
-    for i in nombres:
-        for x in act:
-            
-            if str(i) == "['"+str(x)+"']":
-                doc4 = str(doc4[i])
-                doc5 = doc4.replace('[',"")
-                doc6 = doc5.replace(']',"")
-                doc7 = doc6.replace("'","")
-                act.append(doc7)
+    recomend = []
+    for i in docs:
+        for a in no:
+            if str(i) == str(a):
+                recomend.append(str(i))
+
+
+
+    #Encuentra segun tamanio del grupo
+    comp = db.run("MATCH (p:Place)-[:IR]->(c:Companion) WHERE c.type = '"+compan+"' RETURN p.nombre").to_ndarray()
+
+    com = str(comp)
+    com= com.replace('[',"")
+    com = com.replace("']","*")
+    com = com.replace(']',"")
+    com = com.replace("'","")
+    com = com.replace("\n","")
+    com = com.split("*")
+    x = 1
+    entero = ""
+    for i in com:
+        if x != 11:
+            entero = entero + str(x)+". " + i + "\n"
+            x = x + 1
+
+    act.append(str(entero))
+
+
+    #Recomendacion con las tres condiciones ingresadas
+    recomendar = []
+    for i in com:
+        for a in recomend:
+            if str(i) == str(a) and str(i) != "":
+                recomendar.append(str(i))
+
+    rec = str(recomendar)
+    rec= rec.replace('[',"")
+    rec = rec.replace(']',"")
+    rec = rec.replace("'","")
+    rec = rec.replace("\n","")
+    rec = rec.split(",")
+
+    x = 1
+    entero = ""
+    for i in rec:
+        if x != 11:
+            entero = entero + str(x)+". " + i.lstrip() + "\n"
+            x = x + 1
+
+    act.append(str(entero))
+
+    nombre1 = ""
+    nombre2 = ""
+    nombre3 = ""
+    if len(rec) >= 1:
+        nombre1 = str(rec[0]).lstrip()
+        if len(rec) >= 2:
+            nombre2 = str(rec[1]).lstrip()
+            if len(rec) >= 3:
+                nombre3 = str(rec[2]).lstrip()
+        
+    dir1 = db.run("MATCH (p:Place) WHERE p.nombre = '"+nombre1+"' RETURN p.direccion").to_ndarray()
+    n1 = str(dir1)
+    n1= n1.replace('[',"")
+    n1 = n1.replace(']',"")
+    n1 = n1.replace("'","")
+    entero = "Direccion de " + nombre1+": "+n1 + "\n"
+    act.append(str(entero))
+
+    tel1 = db.run("MATCH (p:Place) WHERE p.nombre = '"+nombre1+"' RETURN p.telefono").to_ndarray()
+    n1 = str(tel1)
+    n1= n1.replace('[',"")
+    n1 = n1.replace(']',"")
+    n1 = n1.replace("'","")
+    entero = "Telefono de " + nombre1+": "+n1 + "\n"
+    act.append(str(entero))
+
+    if len(rec) >2:
+        dir1 = db.run("MATCH (p:Place) WHERE p.nombre = '"+nombre2+"' RETURN p.direccion").to_ndarray()
+        n1 = str(dir1)
+        n1= n1.replace('[',"")
+        n1 = n1.replace(']',"")
+        n1 = n1.replace("'","")
+        entero = "Direccion de " + nombre2+": "+n1 + "\n"
+        act.append(str(entero))
+
+        tel1 = db.run("MATCH (p:Place) WHERE p.nombre = '"+nombre2+"' RETURN p.telefono").to_ndarray()
+        n1 = str(tel1)
+        n1= n1.replace('[',"")
+        n1 = n1.replace(']',"")
+        n1 = n1.replace("'","")
+        entero = "Telefono de " + nombre2+": "+n1 + "\n"
+        act.append(str(entero))
+
+        if len(rec) == 3:
+            dir1 = db.run("MATCH (p:Place) WHERE p.nombre = '"+nombre3+"' RETURN p.direccion").to_ndarray()
+            n1 = str(dir1)
+            n1= n1.replace('[',"")
+            n1 = n1.replace(']',"")
+            n1 = n1.replace("'","")
+            entero = "Direccion de " + nombre3+": "+n1 + "\n"
+            act.append(str(entero))
+
+            tel1 = db.run("MATCH (p:Place) WHERE p.nombre = '"+nombre3+"' RETURN p.telefono").to_ndarray()
+            n1 = str(tel1)
+            n1= n1.replace('[',"")
+            n1 = n1.replace(']',"")
+            n1 = n1.replace("'","")
+            entero = "Telefono de " + nombre3+": "+n1 + "\n"
+            act.append(str(entero))
+
     
     result = verificar(act)
     return result
